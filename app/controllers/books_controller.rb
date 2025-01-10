bb542e863be276e3f91d419df90a35dc9da8ea03
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2025-01-10 12:03:11 +0000

    BookのEdit機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -23,6 +24,9 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
