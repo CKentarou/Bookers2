5e3b4e80687f26cf6a7529cfdda0e6af60d6fcc2
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2025-01-10 12:42:03 +0000

    userのedit機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,13 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  def get_image
+  def get_image(size: nil)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-    image
+  
+    size ? image.variant(resize: size).processed : image
   end
   
 end
