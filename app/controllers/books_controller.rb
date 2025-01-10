81ecbd7f870ed4483b1dcf88c016ac5bb7991fe0
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2025-01-10 09:22:41 +0000

    Bookのindexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,33 @@
 class BooksController < ApplicationController
   def index
-  end
-
-  def new
+    @user = current_user
+    @users = User.all
+    @books = Book.all  # すべてのユーザーの投稿を取得
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
