fb26a150ee49509b416297fe0848e2d16d86450f
EC2 Default User <ec2-user@ip-172-31-13-36.ap-northeast-1.compute.internal>
2025-01-10 17:23:56 +0000

    最終調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
   def index
     @user = current_user
     @users = User.all
@@ -14,11 +16,6 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-
-    user = @book.user
-    unless user.id == current_user.id
-      redirect_to books_path
-    end
   end
 
   def create
@@ -36,13 +33,7 @@ class BooksController < ApplicationController
   end
 
   def update
-
     @book = Book.find(params[:id])
-
-    user = @book.user
-    unless user.id == current_user.id
-      redirect_to books_path
-    end
 
     if @book.update(book_params)
       flash[:notice] = 'You have updated book successfully.'
@@ -62,5 +53,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
   def index
     @users = User.all
     @user = current_user
@@ -12,22 +14,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to books_path
-    end
-
     @user = User.find(params[:id])
   end
 
   def update
-
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to books_path
-    end
-
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'You have updated user successfully.'
@@ -41,5 +31,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :image, :introduction)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 end
